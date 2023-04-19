2cb526d186b1f5f27ab3edb83dc4009dcb958a7b
Eugenio Garcia <eugenio5039@gmail.com>
2023-04-18 23:10:18 -0500

    Update - edit category with external api

--- src/components/categories/EditCategory.jsx
+++ src/components/categories/EditCategory.jsx
@@ -1,106 +1,97 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { getCategoryResource, updateProtectedResource } from '../../services/categories.service';
+import { useForm } from 'react-hook-form';
+import { useAuth0 } from '@auth0/auth0-react';
 
-class EditCategory extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      category: {},
+
+export default function EditCategory() {
+  const navigate = useNavigate();
+  const [category, setCategory] = useState([]);
+  const {id} = useParams();
+  const {register, handleSubmit, setValue} = useForm();
+
+  const {getAccessTokenSilently} = useAuth0();
+
+  const getCategory = async (id) => {
+    const token = await getAccessTokenSilently();
+    const {data, error} = await getCategoryResource(token,id);
+
+    if (data) {
+      setCategory(data);
+      setValue('name', data.name);
     }
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.stripHtmlEntities = this.stripHtmlEntities.bind(this)
+    if (error) {
+      setCategory(error); //TODO error handling
+    }
   }
 
-  componentDidMount() {
-    const url = `/api/v1/categories/edit/${this.props.match.params.id}`
-    console.log(url)
-    fetch(url)
-      .then((response) => {
-        console.log(response)
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error('Network response was not ok.')
-      })
-      .then((response) => this.setState({ category: response }))
-      .catch(() => console.log('error'))
-  }
+  useEffect(() => {
+    let isMounted = true;
 
-  stripHtmlEntities(str) {
-    return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;')
-  }
+    const getCategoryInt = async () => {
+      getCategory(id);
 
-  onChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+      if (!isMounted) {
+        return;
+      }
 
-  onSubmit(event) {
-    event.preventDefault()
-    const url = `/api/v1/categories/update/${this.props.match.params.id}`
-    const { name } = this.state
+    };
 
+    getCategoryInt();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [getAccessTokenSilently]);
+  
+  const onFormSubmit = async (data) => {
+    const name = data.name;
+    data.id = id;
     if (name.length == 0) return
 
-    const body = {
-      name,
-    }
+    const accessToken = await getAccessTokenSilently();
 
-    const token = document.querySelector('meta[name="csrf-token"]').content
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'X-CSRF-Token': token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error('Network response was not ok.')
-      })
-      .then((response) => this.props.history.push('/categories'))
-      .catch((error) => console.log(error.message))
+    const {response, error} = updateProtectedResource(accessToken, data);
+
+    if(!error){
+    navigate("/categories");
+    }
   }
 
-  render() {
-    const { category } = this.state
-    return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-sm-12 col-lg-6 offset-lg-3">
-            <h1 className="font-weight-normal mb-5">
-              Edit a category: {category.id}
-            </h1>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label htmlFor="categoryName">Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  defaultValue={category.name}
-                  id="categoryName"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-              </div>
-              <button type="submit" className="btn custom-button mt-3">
-                Edit Category
-              </button>
-              <Link to="/categories" className="btn btn-link mt-3">
-                Back to categories
-              </Link>
-            </form>
-          </div>
+  const onErrors = errors => console.error(errors);
+
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-sm-12 col-lg-6 offset-lg-3">
+          <h1 className="font-weight-normal mb-5">
+            Edit a category: {category.id}
+          </h1>
+          <form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
+            <div className="form-group">
+              <label htmlFor="categoryName">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="categoryName"
+                className="form-control"
+                rules={{ required: true }}
+                {...register('name')}
+              />
+            </div>
+            <button type="submit" className="btn custom-button mt-3">
+              Edit Category
+            </button>
+            <Link to="/categories" className="btn btn-link mt-3">
+              Back to categories
+            </Link>
+          </form>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default EditCategory
--- src/services/categories.service.jsx
+++ src/services/categories.service.jsx
@@ -20,6 +20,24 @@ export const getProtectedResource = async (accessToken) => {
   };
 };
 
+export const getCategoryResource = async (accessToken,id) => {
+  const config = {
+    url: `${apiServerUrl}/api/v1/categories/edit/${id}`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
+
 export const deleteProtectedResource = async (accessToken, id) =>{
   const config = {
     url: `${apiServerUrl}/api/v1/categories/destroy/${id}`,
@@ -49,7 +67,25 @@ export const createProtectedResource = async (accessToken, data) =>{
     data,
   };
 
-  console.log(config);
+  const { data: createdData, error } = await callExternalApi({ config });
+
+  return {
+    data: createdData || null,
+    error,
+  };
+}
+
+export const updateProtectedResource = async (accessToken, data) =>{
+  const body = JSON.stringify(data);
+  const config = {
+  url: `${apiServerUrl}/api/v1/categories/update/${data.id}`,
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    data,
+  };
 
   const { data: createdData, error } = await callExternalApi({ config });
 
