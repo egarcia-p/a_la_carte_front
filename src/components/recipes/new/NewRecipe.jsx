05f40c3a76a001a0e9a67685b7ef1d14a8255174
Eugenio Garcia <eugenio5039@gmail.com>
2023-06-05 23:00:59 -0500

    Fix - Initial version of new recipe

--- src/components/recipes/new/NewRecipe.jsx
+++ src/components/recipes/new/NewRecipe.jsx
@@ -1,221 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+
+import { useAuth0 } from '@auth0/auth0-react'
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
 import Select from 'react-select'
+import { createProtectedResource } from '../../../services/recipes.service'
+import { getProtectedResource } from '../../../services/categories.service'
+//TODO GET SUBCATEGORIES resource
+import { access } from 'fs'
 
-class NewRecipe extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      subtitle: '',
-      servings: '',
-      total_time: '',
-      author: '',
-      category_id: '',
-      subcategory_id: '',
-      categories: [],
-      category: { id: 'blank', name: 'Select a Category' },
-      subcategories: [],
-      subcategory: { id: 'blank', name: 'Select a Subcategory if applies' },
+
+
+export default function  NewRecipe() {
+  const {register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
+  
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getCategoriesAndSubcategories = async (id) => {
+    const accessToken = await getAccessTokenSilently();
+    //TODO get categories and subcategories resource
+    const { categoriesData, errorCategories } = await getProtectedResource(accessToken);
+    const { subcategoriesData, errorSubcategories } = await (accessToken);
+
+    if(categoriesData && subcategoriesData){
+      setCategories(categoriesData);
+      setSubcategories(subcategoriesData);
     }
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.stripHtmlEntities = this.stripHtmlEntities.bind(this)
+    if(errorCategories || errorSubcategories){
+      setCategories(JSON.stringify(errorCategories, null, 2));
+      setSubcategories(JSON.stringify(errorSubcategories, null, 2))
+    }
   }
 
-  componentDidMount() {
-    Promise.all([
-      fetch('/api/v1/categories/index'),
-      fetch('/api/v1/subcategories/index'),
-    ])
-      .then(([res1, res2]) => {
-        return Promise.all([res1.json(), res2.json()])
-      })
-      .then(([res1, res2]) => {
-        this.setState({
-          categories: res1,
-          subcategories: res2,
-        })
-      })
-  }
+  useEffect(() => {
+    let isMounted = true;
 
-  stripHtmlEntities(str) {
-    return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;')
-  }
+    const getInitialInfo = async () => {
+      //GET categories and subcategories
+      getCategoriesAndSubcategories();
 
-  onChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
-
-  handleSelectChangeCategory = (selectedOption) => {
-    console.log('selected: ' + selectedOption.name)
-    this.setState({ category_id: selectedOption.id })
-    this.setState({ category: selectedOption })
-  }
-
-  handleSelectChangeSubcategory = (selectedOption) => {
-    console.log('selected: ' + selectedOption.name)
-    this.setState({ subcategory_id: selectedOption.id })
-    this.setState({ subcategory: selectedOption })
-  }
-
-  onSubmit(event) {
-    event.preventDefault()
-    const url = '/api/v1/recipes/create'
-    const {
-      title,
-      subtitle,
-      servings,
-      total_time,
-      author,
-      category_id,
-      subcategory_id,
-    } = this.state
-
-    if (
-      title.length == 0 ||
-      subtitle.length == 0 ||
-      servings.length == 0 ||
-      total_time.length == 0 ||
-      author.length == 0 ||
-      category_id.length == 0 ||
-      subcategory_id.length == 0
-    )
-      return
-
-    const body = {
-      title,
-      subtitle,
-      // instruction: instruction.replace(/\n/g, "<br> <br>"),
-      servings,
-      total_time,
-      author,
-      category_id,
-      subcategory_id,
+      if (!isMounted) {
+        return;
+      }
     }
 
-    const token = document.querySelector('meta[name="csrf-token"]').content
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'X-CSRF-Token': token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error('Network response was not ok.')
-      })
-      .then((response) => this.props.history.push(`/recipe/${response.id}`))
-      .catch((error) => console.log(error.message))
+    getInitialInfo();
+    return () => {
+      isMounted = false;
+    }
+  }, [getAccessTokenSilently])
+
+  const onFormSubmit   = async (data) => {
+    const token = await getAccessTokenSilently();
+
+    const {response, error} = createProtectedResource(token, data);
+
+    if (!error) {
+      navigate('/recipes');
+    }
   }
 
-  render() {
-    const { categories, subcategories, category, subcategory } = this.state
-    return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-sm-12 col-lg-6 offset-lg-3">
-            <h1 className="font-weight-normal mb-5">
-              Add a new recipe to our awesome recipe collection.
-            </h1>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label htmlFor="recipeTitle">Title</label>
-                <input
-                  type="text"
-                  name="title"
-                  id="recipeTitle"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeSubtitle">Subtitle</label>
-                <input
-                  type="text"
-                  name="subtitle"
-                  id="recipeSubtitle"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-                <small id="subtitleHelp" className="form-text text-muted">
-                  Subtitle is optional in support of the title of the recipe.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeServings">Servings</label>
-                <input
-                  type="text"
-                  name="servings"
-                  id="recipeServings"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeTotalTime">Total Time</label>
-                <input
-                  type="text"
-                  name="total_time"
-                  id="recipeTotalTime"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeAuthor">Author</label>
-                <input
-                  type="text"
-                  name="author"
-                  id="recipeAuthor"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeCategory">Category</label>
-                <Select
-                  onChange={this.handleSelectChangeCategory}
-                  name="category_id"
-                  value={category}
-                  options={categories}
-                  getOptionLabel={(option) => `${option.name}`}
-                  getOptionValue={(option) => `${option.id}`}
-                ></Select>
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeSubcategory">Subcategory</label>
-                <Select
-                  name="subcategory_id"
-                  onChange={this.handleSelectChangeSubcategory}
-                  value={subcategory}
-                  options={subcategories}
-                  getOptionLabel={(option) => `${option.name}`}
-                  getOptionValue={(option) => `${option.id}`}
-                ></Select>
-              </div>
-              <button type="submit" className="btn custom-button mt-3">
-                Create Recipe
-              </button>
-              <Link to="/recipes" className="btn btn-link mt-3">
-                Back to recipes
-              </Link>
-            </form>
-          </div>
+  const onErrors = (errors) => console.log(errors);
+
+
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-sm-12 col-lg-6 offset-lg-3">
+          <h1 className="font-weight-normal mb-5">
+            Add a new recipe to our awesome recipe collection.
+          </h1>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default NewRecipe
